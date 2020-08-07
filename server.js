0e730ed854829f8a7f23e2c6b19239544640d1f3
Siddhant Jain <siddhantjain15298@gmail.com>
2020-08-07 22:07:58 +0530

    setup completion of layout and partials

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 //require Express
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
 const port = 8080;
 
+
+app.use(expressLayouts);
 //use express router
 ////////URL,/////Path for routes
 app.use('/',require('./routes'));
