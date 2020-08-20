6856ad9b6a96ec76fe53a783495daf107746dd01
Siddhant Jain <siddhantjain15298@gmail.com>
2020-08-20 14:15:50 +0530

    Setup Post Schema in model file

---
+++ models/post.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+	content:{
+		type:string,
+		required: true,
+	}
+	//refer to user schema
+	user:{
+		type:mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	}
+
+},{
+	timestamps: true
+});
+
+const Post = mongoose.model('Post',postSchema);
+
+
+module.exports = Post;
