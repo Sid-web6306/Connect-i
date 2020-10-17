6112ea384fec7f96c451b9c225c44771793523a2
unknown <siddhantjain15298@gmail.com>
2020-10-17 16:19:40 +0530

    Adding post_Api

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,52 @@
-module.exports.index = (req,res)=>{
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comments');
+
+
+module.exports.index = async (req,res)=>{
+
+    let posts = await Post.find({})
+    .sort('-createdAt')
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    })
+
+
     return res.json(200, {
         message: "Lists of Posts",
-        posts: []
+        posts: posts
     })
+}
+
+
+module.exports.destroy =async (req,res)=>{
+	try{
+		let post = await Post.findById(req.params.id);
+
+		//.id means converting the object id into string)
+		// if(post.user == req.user.id){
+			post.remove();
+			await Comment.deleteMany({post:req.params.id})
+			res.json(200,{
+                message:"Posts and associated comments deleted successfully!"
+            })
+			// req.flash('success','Post Deleted Successfully!');
+			return res.redirect('back');
+
+		// }else{
+		// 	req.flash('Error','Post Cannot Deleted');
+		// 	return res.redirect('back');
+		// }
+	}catch(err){
+		console.log("Error in post_controller/destroy: ",err);
+		// req.flash('error',err);
+		return res.json(500, {
+            message:"Internal Server Error"
+        });
+	}
+	
+
 }
